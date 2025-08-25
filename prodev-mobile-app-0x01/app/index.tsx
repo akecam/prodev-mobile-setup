9b74aff3b6adaec1d7a93aea90e3b67f1e16083b
Donald Umeh <donaldumeh54@gmail.com>
2025-08-25 17:13:35 +0100

    prodev_mobile_app

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -6,12 +6,12 @@ export default function Index() {
       <Text>Entry Screen - Awesome</Text>
       <View>
         <Text style={styles.largeText}>
-          TypeScript is Great If you Practice More
+          Typescript is great if you practice more
         </Text>
         <Text style={styles.mediumText}>
-          React Native provides you single code base for cross platforms
+          React Native provides you a single codebase for cross platforms
         </Text>
-        <Text style={styles.smallText}>I am Awesome</Text>
+        <Text style={styles.smallText}>ALX is awesome</Text>
       </View>
     </View>
   );
